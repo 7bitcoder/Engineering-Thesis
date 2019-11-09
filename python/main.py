f784eb1174257d2fe9545ae51fab1c24f6937de2
Sylwester Dawida <Sylwo103@interia.pl>
2019-11-09 15:55:53 +0100

    update'

--- python/main.py
+++ python/main.py
@@ -1,1 +1,17 @@
+from __future__ import print_function
 import numpy as np
+import torch
+from torchvision import transforms, datasets
+
+x = torch.tensor([[1,2,3,4,5],[6,7,8,1,2]], dtype=torch.long);
+y = torch.full_like(x,5);
+print(torch.cuda.is_available())
+print(x.view([1,-1]))
+
+train = datasets.MNIST("", train= True, download= True,
+                       transform= transforms.Compose([transforms.ToTensor()]))
+
+
+test = datasets.MNIST("", train= False, download= True,
+                       transform= transforms.Compose([transforms.ToTensor()]))
+ trainset =  train.
