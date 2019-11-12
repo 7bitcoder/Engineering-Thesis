dee3af650bf8d409ec071832b9bc70a4e131de79
Sylwester Dawida <Sylwo103@interia.pl>
2019-11-12 22:36:49 +0100

    NN beta with gpu support

--- python/main.py
+++ python/main.py
@@ -1,22 +1,142 @@
-from __future__ import print_function
-import numpy as np
-import torch
-from torchvision import transforms, datasets
+import os
 import cv2
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(2)
-print(vc.get(5))
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+REBUILD_DATA = False
+class DogsVSCats():
+    IMG_SIZE = 50
+    CATS = "PetImages/Cat"
+    DOGS = "PetImages/Dog"
+    TESTING = "PetImages/Testing"
+    LABELS = {CATS: 0, DOGS: 1}
+    training_data = []
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    print(frame.shape)
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-cv2.destroyWindow("preview")
+    catcount = 0
+    dogcount = 0
+
+    def make_training_data(self):
+        for label in self.LABELS:
+            print(label)
+            for f in tqdm(os.listdir(label)):
+                if "jpg" in f:
+                    try:
+                        path = os.path.join(label, f)
+                        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
+                        self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])  # do something like print(np.eye(2)[1]), just makes one_hot
+                        #print(np.eye(2)[self.LABELS[label]])
+
+                        if label == self.CATS:
+                            self.catcount += 1
+                        elif label == self.DOGS:
+                            self.dogcount += 1
+
+                    except Exception as e:
+                        pass
+                        #print(label, f, str(e))
+
+        np.random.shuffle(self.training_data)
+        np.save("training_data.npy", self.training_data)
+        print('Cats:',dogsvcats.catcount)
+        print('Dogs:',dogsvcats.dogcount)
+
+if REBUILD_DATA:
+    dogsvcats = DogsVSCats()
+    dogsvcats.make_training_data()
+
+training_data = np.load("training_data.npy", allow_pickle=True)
+print(len(training_data))
+
+import torch
+
+X = torch.Tensor([i[0] for i in training_data]).view(-1,50,50)
+X = X/255.0
+y = torch.Tensor([i[1] for i in training_data])
+
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__() # just run the init of parent class (nn.Module)
+        self.conv1 = nn.Conv2d(1, 32, 5) # input is 1 image, 32 output channels, 5x5 kernel / window
+        self.conv2 = nn.Conv2d(32, 64, 5) # input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 kernel / window
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50,50).view(-1,1,50,50)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512) #flattening.
+        self.fc2 = nn.Linear(512, 2) # 512 in, 2 out bc we're doing 2 classes (dog vs cat).
+
+    def convs(self, x):
+        # max pooling over 2x2
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x) # bc this is our output layer. No activation here.
+        return F.softmax(x, dim=1)
+
+
+net = Net()
+print(net)
+
+import torch.optim as optim
+
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+loss_function = nn.MSELoss()
+
+VAL_PCT = 0.1  # lets reserve 10% of our data for validation
+val_size = int(len(X)*VAL_PCT)
+print(val_size)
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+
+test_X = X[-val_size:]
+test_y = y[-val_size:]
+print(len(train_X), len(test_X))
+
+BATCH_SIZE = 100
+EPOCHS = 10
+
+for epoch in range(EPOCHS):
+    for i in tqdm(range(0, len(train_X), BATCH_SIZE)): # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
+        #print(f"{i}:{i+BATCH_SIZE}")
+        batch_X = train_X[i:i+BATCH_SIZE].view(-1, 1, 50, 50)
+        batch_y = train_y[i:i+BATCH_SIZE]
+
+        net.zero_grad()
+
+        outputs = net(batch_X)
+        loss = loss_function(outputs, batch_y)
+        loss.backward()
+        optimizer.step()    # Does the update
+
+    print(f"Epoch: {epoch}. Loss: {loss}")
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for i in tqdm(range(len(test_X))):
+        real_class = torch.argmax(test_y[i])
+        net_out = net(test_X[i].view(-1, 1, 50, 50))[0]  # returns a list,
+        predicted_class = torch.argmax(net_out)
+
+        if predicted_class == real_class:
+            correct += 1
+        total += 1
+print("Accuracy: ", round(correct/total, 3))
---
+++ python/mainCuda.py
@@ -0,0 +1,188 @@
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+
+
+REBUILD_DATA = False # set to true to one once, then back to false unless you want to change something in your training data.
+
+class DogsVSCats():
+    IMG_SIZE = 50
+    CATS = "PetImages/Cat"
+    DOGS = "PetImages/Dog"
+    TESTING = "PetImages/Testing"
+    LABELS = {CATS: 0, DOGS: 1}
+    training_data = []
+
+    catcount = 0
+    dogcount = 0
+
+    def make_training_data(self):
+        for label in self.LABELS:
+            print(label)
+            for f in tqdm(os.listdir(label)):
+                if "jpg" in f:
+                    try:
+                        path = os.path.join(label, f)
+                        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
+                        self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])  # do something like print(np.eye(2)[1]), just makes one_hot
+                        #print(np.eye(2)[self.LABELS[label]])
+
+                        if label == self.CATS:
+                            self.catcount += 1
+                        elif label == self.DOGS:
+                            self.dogcount += 1
+
+                    except Exception as e:
+                        pass
+                        #print(label, f, str(e))
+
+        np.random.shuffle(self.training_data)
+        np.save("training_data.npy", self.training_data)
+        print('Cats:',dogsvcats.catcount)
+        print('Dogs:',dogsvcats.dogcount)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50,50).view(-1,1,50,50)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 2)
+
+    def convs(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2,2))
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.softmax(x, dim=1)
+
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")  # you can continue going on here, like cuda:1 cuda:2....etc.
+    print("Running on the GPU")
+else:
+    device = torch.device("cpu")
+    print("Running on the CPU")
+
+
+if REBUILD_DATA:
+    dogsvcats = DogsVSCats()
+    dogsvcats.make_training_data()
+
+training_data = np.load("training_data.npy", allow_pickle=True)
+print(len(training_data))
+
+
+
+X = torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50)
+X = X/255.0
+y = torch.Tensor([i[1] for i in training_data])
+
+VAL_PCT = 0.1
+val_size = int(len(X)*VAL_PCT)
+print(val_size)
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+
+test_X = X[-val_size:]
+test_y = y[-val_size:]
+
+print(len(train_X))
+print(len(test_X))
+
+def test(size=32):
+    X, y = test_X[:size], test_y[:size]
+    val_acc, val_loss = fwd_pass(X.view(-1, 1, 50, 50).to(device), y.to(device))
+    return val_acc, val_loss
+
+def batch_test(net):
+    BATCH_SIZE = 100
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        #np.random.shuffle(test_X)
+        #np.random.shuffle(test_y)
+
+        batch_X = test_X[:BATCH_SIZE].view(-1,1,50,50)
+        batch_y = test_y[:BATCH_SIZE]
+
+        batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+
+        net.zero_grad()
+        outputs = net(batch_X)
+
+        matches  = [torch.argmax(i)==torch.argmax(j) for i, j in zip(outputs, batch_y)]
+        acc = matches.count(True)/len(matches)
+
+        print("Test Accuracy:", round(acc, 3))
+
+def fwd_pass(X, y, train=False):
+
+    if train:
+        net.zero_grad()
+    outputs = net(X)
+    matches  = [torch.argmax(i)==torch.argmax(j) for i, j in zip(outputs, y)]
+    acc = matches.count(True)/len(matches)
+    loss = loss_function(outputs, y)
+
+    if train:
+        loss.backward()
+        optimizer.step()
+
+    return acc, loss
+
+MODEL_NAME = f"model-{int(time.time())}"  # gives a dynamic model name, to just help with things getting messy over time.
+net = Net().to(device)
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+loss_function = nn.MSELoss()
+
+print(MODEL_NAME)
+
+
+def train(net):
+    BATCH_SIZE = 100
+    EPOCHS = 30
+
+    with open("model.log", "a") as f:
+        for epoch in range(EPOCHS):
+            for i in tqdm(range(0, len(train_X), BATCH_SIZE)):
+                batch_X = train_X[i:i+BATCH_SIZE].view(-1,1,50,50)
+                batch_y = train_y[i:i+BATCH_SIZE]
+
+                batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+
+                acc, loss = fwd_pass(batch_X, batch_y, train=True)
+
+                #print(f"Acc: {round(float(acc),2)}  Loss: {round(float(loss),4)}")
+                #f.write(f"{MODEL_NAME},{round(time.time(),3)},train,{round(float(acc),2)},{round(float(loss),4)}\n")
+                # just to show the above working, and then get out:
+                if i % 50 == 0:
+                    val_acc, val_loss = test(size=100)
+                    #print(val_acc, float(val_loss))
+                    f.write(f"{MODEL_NAME},{round(time.time(),3)},{round(float(acc),2)},{round(float(loss), 4)},{round(float(val_acc),2)},{round(float(val_loss),4)},{epoch}\n")
+train(net)
